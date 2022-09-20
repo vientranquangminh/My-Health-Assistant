a23e5a1482d68c358801db2efa8dca6397b91c14
dominhthanh0206 <dominhthanh0206@gmail.com>
2022-09-21 00:14:42 +0700

    Update AppBar Actions

--- lib/src/widgets/app_bar_action.dart
+++ lib/src/widgets/app_bar_action.dart
@@ -5,26 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class AppBarAction extends StatelessWidget {
-  static const actionHomePage = 'actionHomePage';
-
-
   final String action;
 
   const AppBarAction(this.action, {super.key});
   @override
   Widget build(BuildContext context) {
-    switch (action) {
-      case actionHomePage:
-        return IconButton(
-            onPressed: () {
-              log("HomePage");
-            },
-            icon: SvgPicture.asset(''));
-      default:
-        return IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(""),
-        );
-    }
+    return IconButton(
+        onPressed: () {
+          log("HomePage");
+        },
+        icon: SvgPicture.asset(action));
   }
 }
