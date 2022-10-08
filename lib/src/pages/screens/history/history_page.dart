45e471b785ef2e1fb2a3e0001b753f7ebb48002a
dominhthanh0206 <dominhthanh0206@gmail.com>
2022-10-08 23:23:42 +0700

    Fix Message Screen

--- lib/src/pages/screens/history/history_page.dart
+++ lib/src/pages/screens/history/history_page.dart
@@ -12,60 +12,71 @@ class HistoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: MyColors.whiteText,
-          elevation: 0.0,
-          leading: SvgPicture.asset('assets/images/main_icon.svg',
-              color: Colors.blue),
-          title: const Text(
-            "Articles",
-            style: TextStyle(color: Colors.black),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset('assets/images/schedule_page/search.svg'),
-                color: Colors.black),
-            IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset('assets/images/schedule_page/more.svg'),
-                color: Colors.black)
-          ]),
+            backgroundColor: MyColors.whiteText,
+            elevation: 0.0,
+            leading: SvgPicture.asset('assets/images/main_icon.svg',
+                color: Colors.blue),
+            title: const Text(
+              "Message",
+              style: TextStyle(color: Colors.black),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset(
+                      'assets/images/schedule_page/search.svg'),
+                  color: Colors.black),
+              IconButton(
+                  onPressed: () {},
+                  icon:
+                      SvgPicture.asset('assets/images/schedule_page/more.svg'),
+                  color: Colors.black)
+            ]),
         body: Padding(
-          padding: const EdgeInsets.symmetric(vertical:8.0, horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
           child: ListView.builder(
             itemCount: recentChats.length,
             itemBuilder: (context, index) {
               return GestureDetector(
-                 onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return ChatRoom(
-                              user: recentChats[index].sender,
-                            );
-                          }));
-                        },
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ChatRoom(
+                      user: recentChats[index].sender,
+                    );
+                  }));
+                },
                 child: Container(
                     margin: const EdgeInsets.only(top: 24),
                     child: Row(
                       children: [
                         CircleAvatar(
                           radius: 25,
-                          backgroundImage: AssetImage(recentChats[index].avatar!),
+                          backgroundImage:
+                              AssetImage(recentChats[index].avatar!),
                         ),
                         const SizedBox(width: 20),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              recentChats[index].sender.name,
-                              style: MyFontStyles.blackColorH1,
+                        Flexible(
+                          flex: 6,
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  overflow: TextOverflow.ellipsis,
+                                  recentChats[index].sender.name,
+                                  style: MyFontStyles.blackColorH1,
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  recentChats[index].text,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: MyFontStyles.normalGreyText
+                                      .copyWith(fontSize: 14),
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 8),
-                            Text(
-                              recentChats[index].text,
-                              style: MyFontStyles.normalGreyText.copyWith(fontSize: 14),
-                            ),
-                          ],
+                          ),
                         ),
                         const Spacer(),
                         Column(
@@ -73,12 +84,14 @@ class HistoryPage extends StatelessWidget {
                           children: [
                             Text(
                               recentChats[index].day!,
-                              style: MyFontStyles.normalGreyText.copyWith(fontSize: 14),
+                              style: MyFontStyles.normalGreyText
+                                  .copyWith(fontSize: 14),
                             ),
                             const SizedBox(height: 6),
                             Text(
                               recentChats[index].time,
-                              style: MyFontStyles.normalGreyText.copyWith(fontSize: 14),
+                              style: MyFontStyles.normalGreyText
+                                  .copyWith(fontSize: 14),
                             ),
                           ],
                         ),
