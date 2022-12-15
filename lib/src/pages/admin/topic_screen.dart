4fab868b0d37eb4a5d31ebf458ea7970c717b6a9
David <zViT@arv.co.th>
2022-12-15 16:04:50 +0700

    remove topic

--- lib/src/pages/admin/topic_screen.dart
+++ lib/src/pages/admin/topic_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_health_assistant/src/data/firebase_firestore/admin/dashboard_functions.dart';
+import 'package:my_health_assistant/src/models/article/article.dart';
 
 import 'package:my_health_assistant/src/pages/admin/widget/appbar_admin.dart';
 
@@ -16,115 +20,118 @@ class TopicScreen extends StatelessWidget {
           const AppBarAdmin(),
           Padding(
             padding: const EdgeInsets.all(40.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Article',
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w800),
-                        ),
-                        Text(
-                          'Manage All Article post',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w800),
-                        )
-                      ]),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8)),
-                  width: MediaQuery.of(context).size.width,
-                  child: DataTable(
-                    columns: const <DataColumn>[
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'ID',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
+            child: StreamBuilder<List<Article>>(
+              stream: DashBoardFunctions.getAllArticles(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong: ${snapshot.error}');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasData) {
+                  List<DataRow> cells = [];
+                  int length = snapshot.data?.length ?? 0;
+                  for (int i = 0; i < length; i++) {
+                    cells.add(
+                      DataRow(
+                        cells: [
+                          DataCell(Text('$i')),
+                          DataCell(Text('${snapshot.data?[i].title}')),
+                          DataCell(Text('${snapshot.data?[i].time}')),
+                          DataCell(Text('${snapshot.data?[i].doctorName}')),
+                          DataCell(IconButton(
+                            onPressed: () {
+                              FirebaseFirestore.instance.collection('articles').doc(snapshot.data?[i].key).delete();
+                            },
+                            icon: const Icon(CupertinoIcons.xmark_circle_fill),
+                            iconSize: 20,
+                          )),
+                        ],
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Article Title',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
+                    );
+                  }
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Article',
+                                style: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.w800),
+                              ),
+                              Text(
+                                'Manage All Article post',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w800),
+                              )
+                            ]),
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Created At',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
+                      const SizedBox(
+                        height: 20,
                       ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            'Created by',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
+                        width: MediaQuery.of(context).size.width,
+                        child: DataTable(
+                          columns: const <DataColumn>[
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'ID',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Article Title',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Created At',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Created by',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  '',
+                                ),
+                              ),
+                            ),
+                          ],
+                          rows: cells
                         ),
-                      ),
-                      DataColumn(
-                        label: Expanded(
-                          child: Text(
-                            '',
-                          ),
-                        ),
-                      ),
+                      )
                     ],
-                    rows: <DataRow>[
-                      DataRow(
-                        cells: [
-                          const DataCell(Text('1')),
-                          const DataCell(Text('How to do this one?')),
-                          const DataCell(Text('25/11/2022')),
-                          const DataCell(Text('Đỗ Minh Thành')),
-                          DataCell(IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                  CupertinoIcons.xmark_circle_fill))),
-                        ],
-                      ),
-                      DataRow(
-                        cells: [
-                          const DataCell(Text('2')),
-                          const DataCell(Text('Flutter is so amazing')),
-                          const DataCell(Text('27/11/2022')),
-                          const DataCell(Text('Viên Quang Minh')),
-                          DataCell(IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                  CupertinoIcons.xmark_circle_fill))),
-                        ],
-                      ),
-                      DataRow(
-                        cells: [
-                          const DataCell(Text('3')),
-                          const DataCell(Text('Is C++ important?')),
-                          const DataCell(Text('27/11/2022')),
-                          const DataCell(Text('Nguyen Son')),
-                          DataCell(IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                  CupertinoIcons.xmark_circle_fill))),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                  );
+                }
+                return Container();
+              },
+              // child:
             ),
           ),
         ],
