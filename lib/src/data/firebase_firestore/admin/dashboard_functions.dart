211ce79cd359915223100fed739a32e22968a9d1
David <zViT@arv.co.th>
2022-12-30 14:13:12 +0700

    add manage appointments functions

--- lib/src/data/firebase_firestore/admin/dashboard_functions.dart
+++ lib/src/data/firebase_firestore/admin/dashboard_functions.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_health_assistant/src/models/appointment/appointment.dart';
 import 'package:my_health_assistant/src/models/article/article.dart';
 import 'package:my_health_assistant/src/models/users/doctor.dart';
 import 'package:my_health_assistant/src/models/users/patient.dart';
@@ -20,5 +21,11 @@ class DashBoardFunctions {
     return FirebaseFirestore.instance.collection('doctors').snapshots().map(
         (snapshot) =>
             snapshot.docs.map((doc) => Doctor.fromJson(doc.data())).toList());
+  }
+  
+  static Stream<List<Appointment>> getAllAppointment(){
+    return FirebaseFirestore.instance.collection('appointments').snapshots().map(
+        (snapshot) =>
+            snapshot.docs.map((doc) => Appointment.fromJson(doc.data())).toList()); 
   }
 }
--- lib/src/pages/admin/appointment.dart
+++ lib/src/pages/admin/appointment.dart
@@ -1,5 +1,11 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:my_health_assistant/src/data/firebase_firestore/admin/dashboard_functions.dart';
+import 'package:my_health_assistant/src/models/appointment/appointment.dart';
 import 'package:my_health_assistant/src/styles/font_styles.dart';
 
 import 'widget/appbar_admin.dart';
@@ -22,96 +28,119 @@ class _AppointmentAdminScreenState extends State<AppointmentAdminScreen> {
           const AppBarAdmin(),
           Padding(
             padding: const EdgeInsets.all(40.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Appointment',
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w800),
-                        ),
-                        Text(
-                          'Manage All Appointment',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w800),
-                        )
-                      ]),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  height: MediaQuery.of(context).size.height / 1.5,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8)),
-                  child: DataTable(
-                    columns: <DataColumn>[
-                      DataColumn(
-                        label: Text('Date',
-                            style: MyFontStyles.blackColorH1
-                                .copyWith(fontWeight: FontWeight.bold)),
+            child: StreamBuilder<List<Appointment>>(
+              stream: DashBoardFunctions.getAllAppointment(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong ${snapshot.error}');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasData) {
+                  List<Appointment> appointments = snapshot.data ?? [];
+                  appointments.sort((a, b) {
+                    DateTime aDate = DateFormat('dd-MM-yyyy HH:mm')
+                        .parse('${a.date} ${a.time}');
+                    log(aDate.toString());
+                    DateTime bDate = DateFormat('dd-MM-yyyy HH:mm')
+                        .parse('${b.date} ${b.time}');
+                    return aDate.compareTo(bDate);
+                  });
+                  List<DataRow> cells = [];
+                  int length = snapshot.data?.length ?? 0;
+                  for (int i = 0; i < length; i++) {
+                    cells.add(DataRow(cells: [
+                      DataCell(Text('${appointments[i].date}')),
+                      DataCell(Text('${appointments[i].time}')),
+                      DataCell(Text('${appointments[i].doctorName}')),
+                      const DataCell(Text('department')),
+                      DataCell(Text('${appointments[i].patientName}')),
+                      DataCell(Text('${appointments[i].status}')),
+                      DataCell(IconButton(
+                        onPressed: () {
+                          FirebaseFirestore.instance
+                              .collection('appointments')
+                              .doc(snapshot.data?[i].id)
+                              .delete();
+                        },
+                        icon: const Icon(CupertinoIcons.xmark_circle_fill),
+                        iconSize: 18,
+                      )),
+                    ]));
+                  }
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Appointment',
+                                style: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.w800),
+                              ),
+                              Text(
+                                'Manage All Appointment',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w800),
+                              )
+                            ]),
                       ),
-                      DataColumn(
-                        label: Text('Time',
-                            style: MyFontStyles.blackColorH1
-                                .copyWith(fontWeight: FontWeight.bold)),
-                      ),
-                      DataColumn(
-                        label: Text('Doctor',
-                            style: MyFontStyles.blackColorH1
-                                .copyWith(fontWeight: FontWeight.bold)),
-                      ),
-                      DataColumn(
-                        label: Text('Department',
-                            style: MyFontStyles.blackColorH1
-                                .copyWith(fontWeight: FontWeight.bold)),
-                      ),
-                      DataColumn(
-                        label: Text('Patient',
-                            style: MyFontStyles.blackColorH1
-                                .copyWith(fontWeight: FontWeight.bold)),
-                      ),
-                      DataColumn(
-                          label: Text('Status',
-                              style: MyFontStyles.blackColorH1
-                                  .copyWith(fontWeight: FontWeight.bold))),
-                      const DataColumn(
-                        label: Text(''),
-                      ),
-                    ],
-                    rows: List<DataRow>.generate(listAppointmentAdmin.length,
-                        (int index) {
-                      return DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text(listAppointmentAdmin[index].date)),
-                          DataCell(Text(listAppointmentAdmin[index].time)),
-                          DataCell(Text(listAppointmentAdmin[index].doctor)),
-                          DataCell(
-                              Text(listAppointmentAdmin[index].department)),
-                          DataCell(Text(listAppointmentAdmin[index].patient)),
-                          DataCell(Text(listAppointmentAdmin[index].status)),
-                          DataCell(IconButton(
-                            icon: const Icon(
-                              CupertinoIcons.xmark_circle_fill,
-                              size: 18,
+                      const SizedBox(height: 20),
+                      Container(
+                        height: MediaQuery.of(context).size.height / 1.5,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: DataTable(
+                          columns: <DataColumn>[
+                            DataColumn(
+                              label: Text('Date',
+                                  style: MyFontStyles.blackColorH1
+                                      .copyWith(fontWeight: FontWeight.bold)),
                             ),
-                            onPressed: () {
-                              setState(() {
-                                listAppointmentAdmin.removeAt(index);
-                              });
-                            },
-                          )),
-                        ],
-                      );
-                    }),
-                  ),
-                )
-              ],
+                            DataColumn(
+                              label: Text('Time',
+                                  style: MyFontStyles.blackColorH1
+                                      .copyWith(fontWeight: FontWeight.bold)),
+                            ),
+                            DataColumn(
+                              label: Text('Doctor',
+                                  style: MyFontStyles.blackColorH1
+                                      .copyWith(fontWeight: FontWeight.bold)),
+                            ),
+                            DataColumn(
+                              label: Text('Department',
+                                  style: MyFontStyles.blackColorH1
+                                      .copyWith(fontWeight: FontWeight.bold)),
+                            ),
+                            DataColumn(
+                              label: Text('Patient',
+                                  style: MyFontStyles.blackColorH1
+                                      .copyWith(fontWeight: FontWeight.bold)),
+                            ),
+                            DataColumn(
+                                label: Text('Status',
+                                    style: MyFontStyles.blackColorH1.copyWith(
+                                        fontWeight: FontWeight.bold))),
+                            const DataColumn(
+                              label: Text(''),
+                            ),
+                          ],
+                          rows: cells,
+                        ),
+                      )
+                    ],
+                  );
+                }
+                return Container();
+              },
             ),
           )
         ],
